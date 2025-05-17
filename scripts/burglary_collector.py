94ad83f5508730e2041f36c786c395ed9471b76c
Bilgin Eren <b.eren@student.tue.nl>
2025-05-17 02:02:52 +0200

    Updated burglary_collector.py, correctly adjusts old LSOA codes into new ones. The new dataset grew with 50k cases.

--- scripts/burglary_collector.py
+++ scripts/burglary_collector.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import csv, logging, os, polars as pl
 from concurrent.futures import ProcessPoolExecutor
 from tqdm.contrib.concurrent import process_map
+from collections import defaultdict
 
 # Logging module
 logging.basicConfig(
@@ -11,11 +12,15 @@ logging.basicConfig(
 
 
 def _filter_file(args):
-    path, valid_codes, crime = args
+    path, valid_codes, lsoa_mapping, crime = args
     rows = []
     with open(path, newline="") as f:
         reader = csv.DictReader(f)
         for row in reader:
+            lsoa_code = row["LSOA code"].strip()
+            if lsoa_code in lsoa_mapping:
+                lsoa_code = lsoa_mapping[lsoa_code]
+                row["LSOA code"] = lsoa_code
             if (row["Crime ID"].strip() and
                 row["LSOA code"] in valid_codes and
                 row["Crime type"] == crime):
@@ -23,7 +28,7 @@ def _filter_file(args):
     return rows
 
 def create_file(file_path: str, output_name: str, lsoa_path: str, crime: str = 'Burglary') -> None:
-    """
+    """Create the crime dataset for London burglaries.
 
     Args:
         file_path: Path to the CSV files.
@@ -37,32 +42,38 @@ def create_file(file_path: str, output_name: str, lsoa_path: str, crime: str = '
 
     base_dir = Path(__file__).resolve().parent
     csv_files = list((base_dir / file_path).rglob("*-street.csv"))
+    lsoa_df = pl.read_parquet(lsoa_path)
     valid_codes = set(pl.read_parquet(lsoa_path)["LSOA21CD"])
+
+    # Mapping for changed LSOA codes (S, M, X)
+    lsoa_mapping = {}
+    changes = lsoa_df.filter(pl.col("CHGIND").is_in(["S", "M", "X"]))
+    for row in changes.select(["LSOA11CD", "LSOA21CD"]).iter_rows(named=True):
+        lsoa_mapping[row["LSOA11CD"]] = row["LSOA21CD"]
 
     logging.info(f'Found {len(csv_files)} files.')
     logging.info(f'Found {len(valid_codes)} London LSOA codes')
 
-    with ProcessPoolExecutor(max_workers=os.cpu_count()) as pool:
-        chunks = process_map(
-            _filter_file,
-            [(p, valid_codes, crime) for p in csv_files],
-            max_workers=os.cpu_count(),
-            chunksize=1,
-            desc="Processing files",
-            unit="files"
-        )
+    chunks = process_map(
+        _filter_file,
+        [(p, valid_codes, lsoa_mapping, crime) for p in csv_files],
+        max_workers=os.cpu_count(),
+        chunksize=1,
+        desc="Processing files",
+        unit="files"
+    )
 
-        # Write everything the workers give us into one temporary CSV
-        tmp_csv = base_dir / "temp.csv"
-        with open(tmp_csv, "w", newline="") as out_f:
-            writer = None
-            for rows in chunks:
-                if not rows:
-                    continue
-                if writer is None:
-                    writer = csv.DictWriter(out_f, fieldnames=rows[0].keys())
-                    writer.writeheader()
-                writer.writerows(rows)
+    # Write everything the workers give us into one temporary CSV
+    tmp_csv = base_dir / "temp.csv"
+    with open(tmp_csv, "w", newline="") as out_f:
+        writer = None
+        for rows in chunks:
+            if not rows:
+                continue
+            if writer is None:
+                writer = csv.DictWriter(out_f, fieldnames=rows[0].keys())
+                writer.writeheader()
+            writer.writerows(rows)
 
 
     # Convert to parquet & deduplicate
