50487accb7b9a2d1aab1d7db75ee5ea38fae2751
YigitIbze <167881239+YigitIbze@users.noreply.github.com>
2025-05-28 13:03:04 +0200

    testing

--- xgBoost/data/preprocess.py
+++ xgBoost/data/preprocess.py
@@ -1,6 +1,13 @@
 import pandas as pd
 from typing import Tuple
-from ingest import load_data
+
+
+
+# Paths to raw data
+X_TRAIN_PATH = 'data/raw/x_train.parquet'
+X_TEST_PATH  = 'data/raw/x_test.parquet'
+Y_TRAIN_PATH = 'data/raw/y_train.parquet'
+Y_TEST_PATH  = 'data/raw/y_test.parquet'
 
 
 def clean_data(X: pd.DataFrame) -> pd.DataFrame:
@@ -69,6 +76,22 @@ def preprocess_data(
 
     return X_train_feat, X_test_feat
 
+
+def load_data():
+    """
+    Load train and test datasets from parquet files.
+
+    Returns:
+        X_train (DataFrame)
+        X_test  (DataFrame)
+        y_train (DataFrame or Series)
+        y_test  (DataFrame or Series)
+    """
+    X_train = pd.read_parquet(X_TRAIN_PATH)
+    X_test  = pd.read_parquet(X_TEST_PATH)
+    y_train = pd.read_parquet(Y_TRAIN_PATH)
+    y_test  = pd.read_parquet(Y_TEST_PATH)
+    return X_train, X_test, y_train, y_test
 
 if __name__ == '__main__':
     X_train, X_test, y_train, y_test = load_data()
