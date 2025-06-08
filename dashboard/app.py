f0c36e5aaaaa4288181cde60aa6d0874f6348390
estee <estee.rutten@gmail.com>
2025-06-08 17:06:05 +0200

    application dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,1 +1,20 @@
 import dash
+from dash import dcc, html, Input, Output
+import plotly.express as px
+import pandas as pd
+
+
+#df = pd.read_csv("")
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H2("London Crime Prediction Tool"),
+   
+])
+
+
+   
+# extra callbacks for the map…
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
